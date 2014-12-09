6edd151aa4cae47bddce4ba6088363e482408659
Matteo Valdina <zanfire@live.com>
2014-12-09 17:29:25 +0100

    Minor relayout

--- server/bokuserver/main.py
+++ server/bokuserver/main.py
@@ -2,19 +2,21 @@ import sys
 import argparse
 import logging
 
-from mqtt import MQTTReceiver
+from mqtt import MQTTClient
 from web.webserver import WebServer
-from store.datastore import Datastore
+from datastore import Datastore
 
 if __name__ == "__main__":
   print("Starting store ...")
   ds = Datastore()
   ds.init()
+  print(" ... done")
 
-  print("Starting MQTT broker ...")
-  mqttrecv = MQTTReceiver()
-  mqttrecv.init()
-  mqttrecv.start()
+  print("Starting MQTT Client ...")
+  mqttcli = MQTTClient()
+  mqttcli.init()
+  mqttcli.start()
+  print(" ... done")
 
   print("Starting web server ...")
   websrv = WebServer()
